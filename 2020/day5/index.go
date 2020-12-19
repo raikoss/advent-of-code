4f09666b27fe677640408ed31014064864d835ec
Daniel B. Stoa <danielstoa@outlook.com>
2020-12-19 22:20:42 +0100

    2020 day5 part2

--- 2020/day5/index.go
+++ 2020/day5/index.go
@@ -40,9 +40,7 @@ func decodePart(part string, ceil int, lowerHalfSep string) int {
 	currentValues := data{initialValues.floor, initialValues.ceil}
 
 	for i, char := range part {
-		take := int(float64((initialValues.ceil + 1)) / math.Pow(float64(2), float64(i+1)))
-		// fmt.Printf("Taking %d\n", take)
-		// fmt.Printf("Current values %#v\n", currentValues)
+		take := int(float64((initialValues.ceil + 1)) / math.Pow(float64(2), float64(i+1))) // get half of ceil on i = 0, then 1/4 on i = 1, then 1/8 on i = 2, etc
 		if string(char) == lowerHalfSep {
 			currentValues.ceil = currentValues.ceil - take
 		} else { // char == "B"
@@ -53,23 +51,29 @@ func decodePart(part string, ceil int, lowerHalfSep string) int {
 	return currentValues.floor // or ceil, they are equal at the end
 }
 
-func getSeatID(pass string) int {
+func getPassRowColumn(pass string) (int, int) {
 	rowString := string(pass[:7])
 	columnString := string(pass[7:])
 
 	row := decodePart(rowString, 127, "F")
 	column := decodePart(columnString, 7, "L")
 
-	// fmt.Printf("Code %v equals row %d, column %d\n", pass, row, column)
+	return row, column
+}
 
+func getSeatID(row, column int) int {
 	return row*8 + column
+}
+
+func getSeatIDByPass(pass string) int {
+	return getSeatID(getPassRowColumn(pass))
 }
 
 func getHighestSeatID(lines []string) int {
 	higestSeatID := 0
 
 	for _, pass := range lines {
-		seatID := getSeatID(pass)
+		seatID := getSeatIDByPass(pass)
 
 		if seatID > higestSeatID {
 			higestSeatID = seatID
@@ -79,10 +83,45 @@ func getHighestSeatID(lines []string) int {
 	return higestSeatID
 }
 
+// ugly but gets the job done I guess
+func getMySeatID(lines []string) int {
+	plane := make([][]string, 8) // columns
+	for i := range plane {
+		plane[i] = make([]string, 128) // rows
+	}
+
+	for _, pass := range lines {
+		row, column := getPassRowColumn(pass)
+
+		plane[column][row] = pass
+	}
+
+	// fmt.Printf("The plane is now %#v\n", plane)
+
+	for column := range plane {
+		for row := range plane[column] {
+			if row == 0 || row == 127 {
+				continue // so we don't check out of bounds, seat is not on edge anyways
+			}
+
+			isAtEdge := plane[column][row+1] == "" || plane[column][row-1] == ""
+
+			if plane[column][row] == "" && !isAtEdge {
+				// fmt.Printf("My seat id is %d,%d\n", column, row)
+				return getSeatID(row, column)
+			}
+		}
+	}
+
+	return 0
+}
+
 func main() {
 	lines := readFile("input.txt")
 
 	highestSeatID := getHighestSeatID(lines)
-
 	fmt.Printf("The highest seat id is %d\n", highestSeatID)
+
+	mySeatID := getMySeatID(lines)
+	fmt.Printf("My seat ID is %d\n", mySeatID)
 }
